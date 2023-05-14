61c8b66a7f8989b832ab9169d8f917c3aa8c7213
noneofurbuzz <ugochiike26@gmail.com>
2023-05-14 17:58:02 +0100

    Edit style

--- script.js
+++ script.js
@@ -1,22 +1,22 @@
-//function that sees if length of input is greater than the max length it should be on input and then only returns the from 0 to the max.length
-function maximum(input) {
-    if (input.value.length > input.max.length ) {
-        input.value = input.value.slice(0, input.max.length);
-    }
-}
-function increment(input){
-    input.value = input.value.replace(/\D/g,"")
+function group(input){
+    input.value = input.value.replace(/\D/g,"")//replaces any character that is not a digit with empty string
     var space = "";
     for(let i = 0; i < input.value.length; i = i + 1){
-        if (i % 4 == 0){
+        if (i % 4 == 0){  
             space = space + " ";
         }
         space = space + input.value[i];
     }
-    input.value = space;
-
-
+    input.value = space; 
 }
-
-    
-
+function num(input){
+    input.value = input.value.replace(/\D/g,"");
+}
+function cardname(input){
+    var name = document.querySelector(".card-name");
+    name.textContent = input.value;
+}
+function cardnum(input){
+    var num = document.querySelector(".card-number span");
+    num.textContent = input.value;
+}
